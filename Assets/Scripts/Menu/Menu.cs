dcbe8d8c5df5081d643fdad10df12c7b17759089
e-Smajic <esmajic2@etf.unsa.ba>
2025-05-13 00:34:31 +0200

    Main menu interface added

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class Menu : MonoBehaviour
+{
+    [Header("Main Menu")]
+    public GameObject mainMenuParent;
+    public Button learnButton;
+    public TMP_Text learnText;
+    public Image learnIcon;
+    public Button settingsButton;
+    public TMP_Text settingsText;
+    public Image settingsIcon;
+    public Button exitButton;
+    public TMP_Text exitText;
+    public Image exitIcon;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(WaitForBookOpening());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WaitForBookOpening()
+    {
+        yield return new WaitForSeconds(1f);
+        mainMenuParent.SetActive(true);
+        StartCoroutine(FadeAlpha(0f, 1f, 1f));
+    }
+
+    IEnumerator FadeAlpha(float start, float end, float fadeDuration)
+    {
+        float time = 0f;
+        while (time < fadeDuration)
+        {
+            Image learnButtonBackground = learnButton.GetComponent<Image>();
+            Image settingsButtonBackground = settingsButton.GetComponent<Image>();
+            Image exitButtonBackground = exitButton.GetComponent<Image>();
+
+            float a = Mathf.Lerp(start, end, time / fadeDuration);
+            if (learnIcon != null) learnIcon.color = new Color(learnIcon.color.r, learnIcon.color.g, learnIcon.color.b, a);
+            if (learnText != null) learnText.color = new Color(learnText.color.r, learnText.color.g, learnText.color.b, a);
+            if (learnButtonBackground != null) learnButtonBackground.color = new Color(learnButtonBackground.color.r, learnButtonBackground.color.g, learnButtonBackground.color.b, a);
+
+            if (settingsIcon != null) settingsIcon.color = new Color(settingsIcon.color.r, settingsIcon.color.g, settingsIcon.color.b, a);
+            if (settingsText != null) settingsText.color = new Color(settingsText.color.r, settingsText.color.g, settingsText.color.b, a);
+            if (settingsButtonBackground != null) settingsButtonBackground.color = new Color(settingsButtonBackground.color.r, settingsButtonBackground.color.g, settingsButtonBackground.color.b, a);
+
+            if (exitIcon != null) exitIcon.color = new Color(exitIcon.color.r, exitIcon.color.g, exitIcon.color.b, a);
+            if (exitText != null) exitText.color = new Color(exitText.color.r, exitText.color.g, exitText.color.b, a);
+            if (exitButtonBackground != null) exitButtonBackground.color = new Color(exitButtonBackground.color.r, exitButtonBackground.color.g, exitButtonBackground.color.b, a);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
